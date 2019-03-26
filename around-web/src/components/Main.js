49b877147d9637d75f962b9acbe85437fba5b9f7
haoyul901209 <keithunightmare@gmail.com>
2019-03-26 19:04:53 -0400

    repo

---
+++ around-web/src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderEmpty from 'antd/lib/config-provider/renderEmpty';
+
+class Main extends React.Component{
+    render(){
+        return (
+            <div></div>
+        );
+    }
+}
