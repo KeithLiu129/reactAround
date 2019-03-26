2ac4bfbd87dc0c86cfd81aa8871911fcaa8736ba
haoyul901209 <keithunightmare@gmail.com>
2019-03-26 19:02:53 -0400

    main

--- around-web/src/components/App.js
+++ around-web/src/components/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
 import { TopBar } from './TopBar';
-import logo from '../assets/images/logo.svg';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopBar />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
