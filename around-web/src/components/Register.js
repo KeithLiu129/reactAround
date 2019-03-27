b10a915d99ab3b111b7817b476f4aa85f2c9f534
haoyul901209 <keithunightmare@gmail.com>
2019-03-27 14:14:45 -0400

    first part

--- around-web/src/components/Register.js
+++ around-web/src/components/Register.js
@@ -77,7 +77,7 @@ class RegistrationForm extends React.Component {
     };
    
     return (
-      <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+      <Form {...formItemLayout} onSubmit={this.handleSubmit} className = "register">
         <Form.Item
           label="Username"
         >
